13bdb17401e0ca2f598f373a27cab520b62a598a
savoskin0502 <savoskin0500@gmail.com>
2019-03-01 17:21:34 +0600

    fd

--- web-project/src/app/app-routing.module.ts
+++ web-project/src/app/app-routing.module.ts
@@ -1,17 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {FaqComponent} from './faq/faq.component';
-// import {CommonModule} from '@angular/common';
+import {CommonModule} from '@angular/common';
+import {EducationComponent} from './education/education.component';
 
 const routes: Routes = [
   {
-    path: 'faq', component: FaqComponent
+    // path: 'faq', component: FaqComponent
+    path: 'education', component: EducationComponent,
   }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
-  // declarations: []
+  declarations: []
 })
 export class AppRoutingModule { }
