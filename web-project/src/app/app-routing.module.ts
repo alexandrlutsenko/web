569101542e0d56da98aa38b7af2894ca0eaaa686
Alexandr Lutsenko <sasha1999.luk@gmail.com>
2019-03-02 15:31:52 +0600

    guide2

--- web-project/src/app/app-routing.module.ts
+++ web-project/src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {CommonModule} from '@angular/common';
 import {EducationComponent} from './education/education.component';
+import { GuideComponent } from './guide/guide.component';
 
 const routes: Routes = [
   {
     // path: 'faq', component: FaqComponent
     path: 'education', component: EducationComponent,
+  },
+  {
+    path: 'guide', component: GuideComponent
   }
 ];
 
