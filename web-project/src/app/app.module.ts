3364626e5ebf8406bd087e617067abddeff26595
savoskin0502 <savoskin0500@gmail.com>
2019-03-02 14:04:46 +0600

    comp1.0

--- web-project/src/app/app.module.ts
+++ web-project/src/app/app.module.ts
@@ -4,12 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EducationComponent } from './education/education.component';
+import { GuideComponent } from './guide/guide.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
 
-    EducationComponent
+    EducationComponent,
+
+    GuideComponent,
+
+    HomeComponent
   ],
   imports: [
     BrowserModule,
