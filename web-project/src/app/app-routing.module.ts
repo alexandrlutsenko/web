3e323ad8ec115252ffa7241822b4258003f4272d
Meruert Amambayeva <marylouisekenly@gmail.com>
2019-03-02 14:48:21 +0600

    homeStart

--- web-project/src/app/app-routing.module.ts
+++ web-project/src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {CommonModule} from '@angular/common';
 import {EducationComponent} from './education/education.component';
+import {HomeComponent} from './home/home.component';
 
 const routes: Routes = [
   {
     // path: 'faq', component: FaqComponent
-    path: 'education', component: EducationComponent,
+    path: 'education', component: EducationComponent
+  },
+  {
+    path:'', component:HomeComponent
   }
 ];
 
