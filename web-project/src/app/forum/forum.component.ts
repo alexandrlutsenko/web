08935d8a7bd6ca96135d097d584e589e12139323
savoskin0502 <savoskin0500@gmail.com>
2019-03-03 11:52:09 +0600

    bugsAgainBugs

--- web-project/src/app/forum/forum.component.ts
+++ web-project/src/app/forum/forum.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+declare var require: any;
 
 @Component({
   selector: 'app-forum',
   templateUrl: './forum.component.html',
   styleUrls: ['./forum.component.css']
 })
+
+
 export class ForumComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
   }
-
+  msg: any= require("../../assets/msg_icon.png");
+  msgBlue: any= require("../../assets/msg_icon_blue.png");
 }
