92bc42f7945ba77980b14a98588271f4d0c996a9
Alex <alexbirukova@mail.ru>
2019-03-16 21:01:28 +0600

    moduleEduc

--- web-project/src/app/app.module.ts
+++ web-project/src/app/app.module.ts
@@ -3,18 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { EducationComponent } from './education/education.component';
+// import { EducationComponent } from './education/education.component';
 import { GuideComponent } from './guide/guide.component';
 import { HomeComponent } from './home/home.component';
 import { DictionaryComponent } from './dictionary/dictionary.component';
 import { MaterialsComponent } from './materials/materials.component';
 import { ForumComponent } from './forum/forum.component';
 import { ContactComponent } from './contact/contact.component';
+import { EducationModuleModule } from './education-module/education-module.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EducationComponent,
+    // EducationComponent,
     GuideComponent,
     HomeComponent,
     DictionaryComponent,
@@ -24,7 +25,8 @@ import { ContactComponent } from './contact/contact.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    EducationModuleModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
